bb348af8b524d9d4047f87f33ab3e8bc7555ba62
joseph viernes <josephnviernes@gmail.com>
2026-06-23 14:00:08 +0800

    created a main python script that orchestrate all the pipeline sequence

---
+++ scripts/main.py
@@ -0,0 +1,19 @@
+from data_processor import land_data_processor as transform
+from load_merge import load, merge
+from dotenv import load_dotenv
+import os
+
+def main():
+    load_dotenv()
+    bucket = os.getenv('bucket')
+    folder = os.getenv('folder')
+    invalids_folder = os.getenv('invalids_folder')
+    db_url = os.getenv('DB_URL')
+    staging_table = os.getenv("staging_table")
+
+    latest_report_df = transform(bucket, folder, invalids_folder)
+    load(db_url, staging_table, latest_report_df)
+    merge(db_url)
+
+if __name__ == "__main__": 
+    main()
